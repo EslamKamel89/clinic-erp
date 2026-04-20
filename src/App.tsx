30c26a45357998d51aad51bc0b47abd4f8bd58c4
EslamKamel89 <elsamkamelforex@gmail.com>
2026-04-20 10:00:49 +0200

    feat(i18n): configure namespaces with resources and enable language switching in UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { useTranslation } from "react-i18next";
 import { Button } from "./components/ui/button";
+import i18n from "./shared/lib/localization/i18n";
 
 function App() {
   const { t } = useTranslation();
   return (
     <div>
-      <div className="text-red-500">{t("app_title")}</div>
-      <Button>{t("click")}</Button>
+      <div className="text-red-500">{t("app_title", { ns: "p001" })}</div>
+      <Button
+        onClick={() => {
+          i18n.changeLanguage("ar");
+        }}
+      >
+        {t("click", { ns: "p001" })}
+      </Button>
+      <Button>{t("save")}</Button>
     </div>
   );
 }
--- src/shared/lib/localization/i18n.ts
+++ src/shared/lib/localization/i18n.ts
@@ -1,9 +1,26 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import enP000 from "./resources/en/p000.json";
+import enP001 from "./resources/en/p001.json";
+
+import arP000 from "./resources/ar/p000.json";
+import arP001 from "./resources/ar/p001.json";
 i18n.use(initReactI18next).init({
   lng: "en",
   fallbackLng: "en",
-  resources: {},
+  ns: ["p000", "p001"],
+  // i made the defaultNs p0000 because this is namespace will contain shared keys. but i can't use any other namespace because it's page scope and this will cause issues if i forget to identify namespace and by mistake i get the data from another namespace.
+  defaultNS: "p000",
+  resources: {
+    en: {
+      p000: enP000,
+      p001: enP001,
+    },
+    ar: {
+      p000: arP000,
+      p001: arP001,
+    },
+  },
   interpolation: {
     escapeValue: false,
   },
