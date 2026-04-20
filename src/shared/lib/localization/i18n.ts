9ffbbbc01e5940dc2947ce15f2d7a660df86185e
EslamKamel89 <elsamkamelforex@gmail.com>
2026-04-20 10:18:54 +0200

    refactor(i18n): update default and fallback namespaces configuration

--- src/shared/lib/localization/i18n.ts
+++ src/shared/lib/localization/i18n.ts
@@ -9,8 +9,8 @@ i18n.use(initReactI18next).init({
   lng: "en",
   fallbackLng: "en",
   ns: ["p000", "p001"],
-  // i made the defaultNs p0000 because this is namespace will contain shared keys. but i can't use any other namespace because it's page scope and this will cause issues if i forget to identify namespace and by mistake i get the data from another namespace.
-  defaultNS: "p000",
+  defaultNS: "p001",
+  fallbackNS: "p000",
   resources: {
     en: {
       p000: enP000,
