706cddc83bc84c85cfed02219eae55267c4e6c20
EslamKamel89 <elsamkamelforex@gmail.com>
2026-04-23 22:29:00 +0200

    feat(router): add react-router setup and integrate RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
 import "./app/globals.css";
+import { router } from "./app/router/index.tsx";
 import i18n from "./shared/lib/localization/i18n.ts";
 i18n;
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
