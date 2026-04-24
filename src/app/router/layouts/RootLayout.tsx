8b7e5e34370b7fd03baf34731fb0dffe83519fe4
EslamKamel89 <elsamkamelforex@gmail.com>
2026-04-24 09:18:56 +0300

    feat(router): add base layout components with Outlet and localization initialization

---
+++ src/app/router/layouts/RootLayout.tsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom";
+import useLocalizationInit from "../../../shared/lib/localization/initLocalization";
+
+export const RootLayout = () => {
+  useLocalizationInit();
+  return <Outlet />;
+};
