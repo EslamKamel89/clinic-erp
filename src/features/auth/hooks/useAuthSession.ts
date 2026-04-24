12e53ee0354dd251745bf3ce6abfc8fc5e7d04d9
EslamKamel89 <elsamkamelforex@gmail.com>
2026-04-25 00:37:13 +0300

    feat(auth): handle session hydration in RootLayout and optimize useAuthSession caching logic

--- src/features/auth/hooks/useAuthSession.ts
+++ src/features/auth/hooks/useAuthSession.ts
@@ -5,14 +5,22 @@ import { queryClientKeys } from "../../../shared/lib/query/keys";
 import { meApi } from "../api/me.api";
 import { serializeLoginResponse } from "../api/serializers/login.serializer";
 import { useAuthStore } from "../store/auth.store";
+import type { MenuItem, User } from "../types/auth.types";
 
 export const useAuthSession = () => {
   const token = useAuthStore((s) => s.token);
-  const setToken = useAuthStore((s) => s.setToken);
   const queryClient = useQueryClient();
+  const existingUser = queryClient.getQueryData<User>(
+    queryClientKeys.auth.user,
+  );
+
+  const existingMenu = queryClient.getQueryData<MenuItem[]>(
+    queryClientKeys.auth.menu,
+  );
+  const setToken = useAuthStore((s) => s.setToken);
   const query = useQuery({
     queryKey: queryClientKeys.auth.session,
-    enabled: !!token,
+    enabled: !!token && !existingUser && !existingMenu,
     queryFn: async () => {
       const response = await meApi();
       return serializeLoginResponse(response);
@@ -20,13 +28,15 @@ export const useAuthSession = () => {
     retry: false,
   });
   useEffect(() => {
-    if (!query.data) return;
+    if (!query.isSuccess) return;
     queryClient.setQueryData(queryClientKeys.auth.user, query.data.user);
     queryClient.setQueryData(queryClientKeys.auth.menu, query.data.menu);
-    if (i18n.language !== query.data.language) {
-      i18n.changeLanguage(query.data.language);
-    }
-  }, [query.data, queryClient]);
+    // i will keep the language english fixed for now because there is an issue in the backend, later i will fix it.
+    i18n.changeLanguage("en");
+    // if (i18n.language !== query.data.language) {
+    //   i18n.changeLanguage(query.data.language);
+    // }
+  }, [query.isSuccess, queryClient, query.data?.menu, query.data?.user]);
   useEffect(() => {
     if (query.isError) {
       setToken(null);
