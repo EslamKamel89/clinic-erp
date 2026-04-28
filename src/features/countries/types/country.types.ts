50633f78126c5e3738980f72ade911b52e82a585
EslamKamel89 <elsamkamelforex@gmail.com>
2026-04-28 21:01:00 +0300

    feat(countries): define raw and normalized country types

---
+++ src/features/countries/types/country.types.ts
@@ -0,0 +1,13 @@
+export type CountryRaw = {
+  ID: number;
+  Obj_Name: string;
+  PhoneCode: string;
+  Notes: string;
+};
+
+export type Country = {
+  id: number;
+  name: string;
+  phoneCode: string;
+  notes: string;
+};
